90300142bdb05d9c8a14716b6d87497f7aa61501
JanDonnermayer <jan@donnermayer.de>
2019-11-30 02:37:10 +0100

    Fix SynchronizationContext Issue

--- src/DocumentStores/Internal/JsonFileHandling.cs
+++ src/DocumentStores/Internal/JsonFileHandling.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using DocumentStores.Primitives;
 using Newtonsoft.Json;
@@ -8,18 +9,23 @@ using Newtonsoft.Json;
 namespace DocumentStores.Internal
 {
     internal class JsonFileHandling : IFileHandling
-    {        
-        string IFileHandling.FileExtension<T>() => 
+    {
+        string IFileHandling.FileExtension<T>() =>
             ".json";
 
         string IFileHandling.Subdirectory<T>() =>
            typeof(T).ShortName(true).Replace(">", "}").Replace("<", "{");
 
         async Task IFileHandling.SerializeAsync<T>(StreamWriter sw, T data) =>
-            await sw.WriteAsync(JsonConvert.SerializeObject(data, Formatting.Indented)).ConfigureAwait(false);
+            await sw.WriteAsync(JsonConvert.SerializeObject(data, Formatting.Indented));
 
-        async Task<T> IFileHandling.DeserializeAsync<T>(StreamReader sr)=>
-            JsonConvert.DeserializeObject<T>(await sr.ReadToEndAsync().ConfigureAwait(false));
-
+        async Task<T> IFileHandling.DeserializeAsync<T>(StreamReader sr)
+        {
+            return SynchronizationContext.Current switch
+            {
+                null => JsonConvert.DeserializeObject<T>(await sr.ReadToEndAsync()),
+                _ => await Task.FromResult(JsonConvert.DeserializeObject<T>(sr.ReadToEnd()))
+            };
+        }
     }
 }
