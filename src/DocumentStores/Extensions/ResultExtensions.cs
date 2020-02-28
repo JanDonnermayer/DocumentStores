305be6684f5f3de4e42a9e0a7182753adfca172f
JanDonnermayer <jan@donnermayer.de>
2020-02-28 06:18:09 +0100

    Make code leaner

--- src/DocumentStores/Extensions/ResultExtensions.cs
+++ src/DocumentStores/Extensions/ResultExtensions.cs
@@ -5,7 +5,7 @@ namespace System
 {
     public static class ResultExtensions
     {
-        public static void Match<T>(
+        public static void Handle<T>(
             this Result<T> result,
             Action<T> dataHandler,
             Action<Exception> errorHandler
@@ -21,46 +21,64 @@ namespace System
                 errorHandler(ex!);
         }
 
-        public static V Match<T, V>(
+        public static V Map<T, V>(
             this Result<T> result,
-            Func<T, V> dataHandler,
-            Func<Exception, V> errorHandler
+            Func<T, V> dataMapper,
+            Func<Exception, V> errorMapper
         ) where T : class
         {
             if (result is null) throw new ArgumentNullException(nameof(result));
-            if (dataHandler is null) throw new ArgumentNullException(nameof(dataHandler));
-            if (errorHandler is null) throw new ArgumentNullException(nameof(errorHandler));
+            if (dataMapper is null) throw new ArgumentNullException(nameof(dataMapper));
+            if (errorMapper is null) throw new ArgumentNullException(nameof(errorMapper));
 
             if (result.Try(out var data, out var ex))
-                return dataHandler(data!);
+                return dataMapper(data!);
             else
-                return errorHandler(ex!);
+                return errorMapper(ex!);
         }
 
-        public static async Task MatchAsync<T>(
+        public static async Task HandleAsync<T>(
             this Task<Result<T>> resultTask,
             Action<T> dataHandler,
             Action<Exception> errorHandler
         ) where T : class
         {
             if (resultTask is null) throw new ArgumentNullException(nameof(resultTask));
-            if (dataHandler is null) throw new ArgumentNullException(nameof(dataHandler));
-            if (errorHandler is null) throw new ArgumentNullException(nameof(errorHandler));
 
-            Match(await resultTask.ConfigureAwait(false), dataHandler, errorHandler);
+            Handle(await resultTask.ConfigureAwait(false), dataHandler, errorHandler);
         }
 
-        public static async Task<V> MatchAsync<T, V>(
+        public static async Task<V> MapAsync<T, V>(
             this Task<Result<T>> resultTask,
-            Func<T, V> dataHandler,
-            Func<Exception, V> errorHandler
+            Func<T, V> dataMapper,
+            Func<Exception, V> errorMapper
         ) where T : class
         {
             if (resultTask is null) throw new ArgumentNullException(nameof(resultTask));
-            if (dataHandler is null) throw new ArgumentNullException(nameof(dataHandler));
-            if (errorHandler is null) throw new ArgumentNullException(nameof(errorHandler));
 
-            return Match(await resultTask.ConfigureAwait(false), dataHandler, errorHandler);
+            return Map(await resultTask.ConfigureAwait(false), dataMapper, errorMapper);
+        }
+
+         public static void HandleResult<T>(
+            this Task<Result<T>> resultTask,
+            Action<T> dataHandler,
+            Action<Exception> errorHandler
+        ) where T : class
+        {
+            if (resultTask is null) throw new ArgumentNullException(nameof(resultTask));
+
+            Handle(resultTask.Result, dataHandler, errorHandler);
+        }
+
+        public static V MapResult<T, V>(
+            this Task<Result<T>> resultTask,
+            Func<T, V> dataMapper,
+            Func<Exception, V> errorMapper
+        ) where T : class
+        {
+            if (resultTask is null) throw new ArgumentNullException(nameof(resultTask));
+
+            return Map(resultTask.Result, dataMapper, errorMapper);
         }
     }
 }
