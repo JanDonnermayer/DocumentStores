4699309b81dfe7265ff2bf83eaba9810c02c9c6a
JanDonnermayer <jan@donnermayer.de>
2020-03-01 14:40:24 +0100

    Update SerializationException.cs

--- src/DocumentStores/Internal/Primitives/Exceptions/SerializationException.cs
+++ src/DocumentStores/Internal/Primitives/Exceptions/SerializationException.cs
@@ -4,13 +4,23 @@
 
 namespace DocumentStores
 {
+    /// <summary>
+    /// The Exception that is thrown when serialization failed.
+    /// </summary>
     public class SerializationException : Exception
     {
+        /// <inheritdoc/>
         public SerializationException(string message) : base(message)
         {
         }
 
+        /// <inheritdoc/>
         public SerializationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        /// <inheritdoc/>
+        public SerializationException()
         {
         }
     }
