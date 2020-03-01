ec2b41ab74be7a4ae2a07190967ea612d5570d68
JanDonnermayer <jan@donnermayer.de>
2020-03-01 14:59:37 +0100

    Add test app

---
+++ app/DocumentStores.SampleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DocumentStores.SampleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var store = new JsonFileDocumentStore(
+                password: Prompt("Please enter password!")
+            );
+
+            await store
+                .GetOrAddAsync(
+                    address: Environment.UserName + "_secrets",
+                    addDataAsync: _ => Task.FromResult(Prompt("Please enter secret!"))
+                )
+                .HandleAsync(
+                    dataHandler: Console.WriteLine,
+                    errorHandler: ex => Console.WriteLine(ex.Message)
+                );
+
+            PromptKey("Press any key");
+        }
+
+        private static string Prompt(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        private static void PromptKey(string message)
+        {
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
